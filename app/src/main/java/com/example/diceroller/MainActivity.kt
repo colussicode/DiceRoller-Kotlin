53c82e12f6874e0b08bee2768d96cf755197d585
colussicode <colussidev@gmail.com>
2023-03-30 15:54:32 -0300

    deleting some lines of code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -11,17 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.buttonRoll.setOnClickListener { rollDice() }
+        binding.buttonRoll.setOnClickListener {
+            rollDice()
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        }
     }
     private fun rollDice() {
-        val dice = Dice(6)
-        val secondDice = Dice(10)
+        val dice = Dice(6).roll()
+        val secondDice = Dice(10).roll()
 
-        val secondDiceRollResult = secondDice.roll()
-        val firstDiceRollResult = dice.roll()
-
-        binding.textViewSecond.text = secondDiceRollResult.toString()
-        binding.textViewFirst.text = firstDiceRollResult.toString()
+        binding.textViewSecond.text = dice.toString()
+        binding.textViewFirst.text = secondDice.toString()
     }
 }
 class Dice(private val numSides: Int) {
